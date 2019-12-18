30070959d0eabe5ee1037179ee2452366f428935
Regis Lucas <regishallowed@gmail.com>
2019-12-18 14:28:17 -0300

    primeito commit

---
+++ js/main_js.js
@@ -0,0 +1,10 @@
+//alert("Olá seja Bem Vindo!");
+
+var lista = ['banana', 'laranja', 'maçã'];
+console.log(lista);
+lista.push('goiaba');
+console.log('Alterando a Lista');
+console.log(lista);
+
+lista.pop("1");
+console.log(lista);
